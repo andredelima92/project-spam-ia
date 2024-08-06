2a315e359e8b41017fe0327bf85048c04b89b652
Andre Lima <andredelima92@gmail.com>
2024-08-06 15:57:13 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
